050476d7535812bc5b5cda386e7f7f3fff32a1d1
Simonas Bujenauskas <simonas.bujenauskas@adform.com>
2024-05-15 15:21:13 +0300

    some letters

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ export async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Waiting ${ms} milliseconds zzz...`)
     console.log(`SB SB Waiting ${ms} milliseconds ...`)
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
