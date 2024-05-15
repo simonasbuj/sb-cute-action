8bc06990edbb80a2cf8fee854d46fcb1a2138171
Simonas Bujenauskas <simonas.bujenauskas@adform.com>
2024-05-15 16:08:58 +0300

    something new

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,21 @@ import { wait } from './wait'
 export async function run(): Promise<void> {
   try {
     // const ms: string = core.getInput('milliseconds')
-    const ms: string = core.getInput('sb_input')
+    const sb_input: string = core.getInput('sb_input')
     
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-    core.debug(`this is me sb sb sb`)
-    console.log(`SB SB Waiting ${ms} milliseconds ...`)
+    // core.debug(`Waiting ${ms} milliseconds ...`)
+    // core.debug(`this is me sb sb sb`)
+    // console.log(`SB SB Waiting ${ms} milliseconds ...`)
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+    await wait(parseInt('1000', 10))
     core.debug(new Date().toTimeString())
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
+    core.setOutput('sb_input', sb_input)
+
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
